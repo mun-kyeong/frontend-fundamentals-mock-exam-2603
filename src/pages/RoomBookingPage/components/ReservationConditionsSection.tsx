069851779bc7ae0a1eb3ee8aa9acd80904f93300
mun-kyeong <horse6953@naver.com>
2026-03-22 23:39:21 +0900

    refactor: TIME_SLOTS 상수를 ReservationConditionsSection 컴포넌트로 이동 및 constants.ts 파일 삭제

--- src/pages/RoomBookingPage/components/ReservationConditionsSection.tsx
+++ src/pages/RoomBookingPage/components/ReservationConditionsSection.tsx
@@ -2,7 +2,6 @@ import { Select, Spacing } from '_tosslib/components';
 import { css } from '@emotion/react';
 import { Controller, useFormContext } from 'react-hook-form';
 import { DatePicker } from '../../../shared/components/DatePicker';
-import { TIME_SLOTS } from '../constants';
 import { FieldBlock } from './FieldBlock';
 import { NumberInput } from './NumberInput';
 import { ToggleChipButton } from './ToggleChipButton';
@@ -16,6 +15,17 @@ type Props = {
   equipment: Equipment[];
   onFilterChange: () => void;
 };
+
+export const TIME_SLOTS: string[] = (() => {
+  const slots: string[] = [];
+  for (let h = 9; h <= 20; h++) {
+    slots.push(`${String(h).padStart(2, '0')}:00`);
+    if (h < 20) {
+      slots.push(`${String(h).padStart(2, '0')}:30`);
+    }
+  }
+  return slots;
+})();
 
 export function ReservationConditionsSection(props: Props) {
   const { floors, equipment, onFilterChange } = props;
